c4af90b193d00706e0e2a04b8887a362d1ca5b94
florin marinescu <marinescu.florin.alexandru@gmail.com>
2016-11-19 18:49:45 +0200

    Add contact, reports, about, expense components to app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReportsComponent } from './reports/reports.component';
+import { AboutComponent } from './about/about.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'contact', component: ContactComponent },
+      { path: 'reports', component: ReportsComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'expense', component: ExpenseComponent },
+      { path: '', redirectTo: 'contact', pathMatch: 'full'}
+    ], {useHash: true})
+    ],
+  declarations: [ 
+    AppComponent,
+    ContactComponent,
+    ReportsComponent,
+    AboutComponent,
+    ExpenseComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
