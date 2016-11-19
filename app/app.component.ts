cab8c42e41485239489e4f442135f6e2910d2312
florin marinescu <marinescu.florin.alexandru@gmail.com>
2016-11-19 18:48:25 +0200

    Init app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,47 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    styleUrls: ['app/app.component.css'],
+    template: `
+    <div id="wrapper">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Start Bootstrap
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a [routerLink]="['/expenses']">Expenses</a>
+                </li>
+                <li>
+                    <a href="#">Incoms</a>
+                </li>
+                <li>
+                    <a [routerLink]="['/reports']">Reports</a>
+                </li>
+                <li>
+                    <a [routerLink]="['/about']">About</a>
+                </li>
+                <li>
+                    <a [routerLink]="['/contact']">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <router-outlet></router-outlet>
+                    </div>
+                </div>
+            </div>
+        </div>     
+    </div>
+        `
 })
 export class AppComponent { }
